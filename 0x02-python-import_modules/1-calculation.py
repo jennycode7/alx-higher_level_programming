25fcca23cacec1d2e2f596663c835fcef4313c59
jennycode7 <jennessecosmas7>
2023-11-03 03:13:16 -0700

    calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if _name__ == "__man__":
+    from add_0 import add, sub, div, mul
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
