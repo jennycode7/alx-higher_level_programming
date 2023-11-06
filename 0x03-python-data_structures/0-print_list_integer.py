680133b8d3106d1cb096f0942fd5a703e676f952
jennycode7 <jennessecosmas7>
2023-11-06 02:14:50 -0800

    prnt list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+x = 0
 def print_list_integer(my_list=[]):
-    for x in my_list:
-        print("{}".format(x))
+    for x in range(len(my_list)):
+        print("{}".format(my_print[x]))
+        x += 1
