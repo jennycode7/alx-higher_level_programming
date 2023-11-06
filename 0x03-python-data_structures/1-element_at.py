ebc04f0f803f6c1c76c584cb5d41037abeb41c41
jennycode7 <jennessecosmas7>
2023-11-06 04:35:33 -0800

    element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,6 +5,5 @@ def element_at(my_list, idx):
     """ retrieves element at idx """
     if idx < 0 or idx >= len(my_list):
         return(None)
-    for x in range(len(my_list)):
-        if x == idx:
-            return(my_list[x])
+    
+    return(my_list[idx])
