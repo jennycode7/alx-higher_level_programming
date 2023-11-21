a097cde87671e7ae784563476d28ce0e9024dc22
jennycode7 <jennessecosmas7>
2023-11-20 17:44:30 -0800

    Safe print

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+
+def safe_print_integer(value):
+    """
+    Prints the given integer value.
+    :param value: The integer value to print.
+    :return: The number of printed characters.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
