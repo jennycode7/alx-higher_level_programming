74cde1a8f843bea08488d8fa2b7512b112ce4800
jennycode7 <jennessecosmas7>
2023-11-27 16:20:38 -0800

    crete rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -63,9 +63,9 @@ class Rectangle:
         """
 
         if type(width) is not int:
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if width < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
 
     def check_heigth(self, height):
         """
@@ -74,9 +74,9 @@ class Rectangle:
         """
 
         if type(height) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if height < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
 
     def area(self):
         """
