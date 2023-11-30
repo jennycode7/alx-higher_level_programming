4cfe5fbccb13dc645dfb223b7a833c005b1205d3
jennycode7 <jennessecosmas7>
2023-11-30 05:55:32 -0800

    print number

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for y in range(0, 100):
     if y == 99:
-        print("{:02} ".format(y), end='\n')
+        print("{:02}".format(y), end='\n')
         break
     print("{:02}, ".format(y), end='')
