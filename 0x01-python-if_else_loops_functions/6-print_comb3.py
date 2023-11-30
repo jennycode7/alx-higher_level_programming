8c709752c48b8331198d120fcf6f476ee2ec6d2a
jennycode7 <jennessecosmas7>
2023-11-30 06:14:05 -0800

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for y in range(0, 100):
-  if y / 10 < y % 10:
-    print("{:02}".format(y), end=' ')
-print()
+    if y / 10 < y % 10:
+        if y == 89:
+            print("{:02}".format(y), end='\n')
+            break
+        print("{:02}, ".format(y), end='')
