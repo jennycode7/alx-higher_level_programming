0ded1ef89d3bc9f07d69fa87a6a61fe865b896c1
jennycode7 <jennessecosmas7>
2023-12-02 03:04:51 -0800

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz ", end='')
+    elif x % 3 == 0:
+        print("Fizz ", end='')
+    elif x == 100:
+        print("Buzz", end='\n')
+        break
+    elif x % 5 == 0:
+        print("Buzz ", end='')
+    else:
+        print("{} ".format(x), end='')
