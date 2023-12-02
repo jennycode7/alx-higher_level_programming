0c2c8663f6b5bc3cd7312295ccab23d30c01ba58
jennycode7 <jennessecosmas7>
2023-12-02 03:44:05 -0800

    100-print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    char = chr(i)
-    if i % 2 == 0:
-        print(char.lower(), end='')
-    else:
-        print(char.upper(), end='')
+    print(f"{chr(i).lower() if i % 2 == 0 else chr(i).upper()}", end='')
