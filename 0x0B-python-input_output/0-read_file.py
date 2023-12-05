00e8dcbb6335dcfd15813fdea3a3fe5f6ec82d4b
jennycode7 <jennessecosmas7>
2023-12-05 03:46:43 -0800

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    Reads a file and prints its content.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end='')
