ab3bc0f0e206c733dc025295b7bce5202c224445
jennycode7 <jennessecosmas7>
2023-12-09 03:29:16 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not [[]]:
+    if matrix:
         for row in matrix:
             for x in range(len(row)):
                 if x == len(row) - 1:
-                    print("{:d}".format(row[x]), end='\n')
+                    print("{:d}".format(row[x]), end='')
                 else:
                     print("{:d} ".format(row[x]), end='')
                 x += 1
+            print()
