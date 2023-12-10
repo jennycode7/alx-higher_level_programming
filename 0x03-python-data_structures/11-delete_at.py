3459481e095b52db88c10625b986325fadb5b61f
jennycode7 <jennessecosmas7>
2023-12-10 12:05:47 -0800

    max integer

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,5 +6,7 @@ def delete_at(my_list=[], idx=0):
     """
     if idx < 0 or idx >= len(my_list):
         return my_list  
-    my_list.pop(idx)
-    return my_list
+    for x in my_list:
+        if x == my_list[idx]:
+            my_list.remove(x)
+            return my_list
