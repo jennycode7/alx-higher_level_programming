57c5018aa327cd6577daa0f984dbb077addf55ce
jennycode7 <jennessecosmas7>
2023-12-10 12:16:38 -0800

    max integer

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,7 +5,7 @@ def delete_at(my_list=[], idx=0):
     Deletes the element at the given index.
     """
     if idx < 0 or idx >= len(my_list):
-        return my_list  
+        return my_list
     for x in my_list:
         if x == my_list[idx]:
             my_list.remove(x)
