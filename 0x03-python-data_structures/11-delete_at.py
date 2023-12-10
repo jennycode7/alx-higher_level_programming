b1629b9ba53c0be735dfc81b0c325dc94a1d8571
jennycode7 <jennessecosmas7>
2023-12-10 11:58:37 -0800

    max integer

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """
+    Deletes the element at the given index.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list  
+    my_list.pop(idx)
+    return my_list
