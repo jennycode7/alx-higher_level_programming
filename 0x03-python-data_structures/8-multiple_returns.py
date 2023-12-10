1489989154b2776f8e89e923f497e95606b4ba7d
jennycode7 <jennessecosmas7>
2023-12-10 06:26:35 -0800

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    """
+    Returns a tuple of (sentence, sentence length)
+    """
+    if sentence:
+        return(len(sentence), sentence[0])
+    else:
+        return (0, None)
