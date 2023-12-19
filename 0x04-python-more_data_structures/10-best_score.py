4d158beab539701ee728e3ee1fd6b17f8f9a0fde
jennycode7 <jennessecosmas7>
2023-12-19 05:09:10 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,10 +5,13 @@ def best_score(a_dictionary):
     """
     Returns the key of the dictionary that has the highest value.
     """
-    best_key = None
-    best_value = 0
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_value = value
-            best_key = key
-    return best_key
+    if a_dictionary:
+        best_key = None
+        best_value = 0
+        for key, value in a_dictionary.items():
+            if value > best_value:
+                best_value = value
+                best_key = key
+        return best_key
+    else:
+        return None
