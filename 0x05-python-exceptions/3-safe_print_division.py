9258b93885d0305ac05595420f95432484cc356a
jennycode7 <jennessecosmas7>
2023-12-27 02:56:13 -0800

    safe print

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    Prints the division of two numbers, if b is not zero.
+    """
+    x = None
+    try:
+        x = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(x))
