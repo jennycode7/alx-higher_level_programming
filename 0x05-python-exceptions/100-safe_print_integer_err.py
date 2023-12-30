0aabdff1d9dbe107bd157955ded000c10235bb7c
jennycode7 <jennessecosmas7>
2023-12-30 03:35:18 -0800

    safe integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import sys
+
+def safe_print_integer_err(value):
+    """
+    Prints the integer value to stdout.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
