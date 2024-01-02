46d0894830825a9d315aa8d024c65753d2af366f
jennycode7 <jennessecosmas7>
2024-01-02 14:50:03 -0800

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""
+This is an empty class
+"""
+
+
+class Square:
+    """
+    Describes a square
+    """
+    def __init__(self, size):
+        """
+        Initializes a size
+        """
+        self.__size = size
