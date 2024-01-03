a50bc457318d20194bcb04681a609feb03785d99
jennycode7 <jennessecosmas7>
2024-01-02 16:14:04 -0800

    1-square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+"""
+This is an empty class
+"""
+
+
+class Square:
+    """
+    Describes a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.size = size
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Returns the size
+        """
+        return self.__size
+      
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return self.__size ** 2
