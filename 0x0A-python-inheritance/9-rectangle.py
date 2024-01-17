fc731fb4daa37644a34391079a58939e80c092f8
jennycode7 <jennessecosmas7@gmail.com>
2024-01-17 04:17:46 -0800

    recatnglr 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A module
+"""
+BaseGeometry = __import__('').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle inherits from BaseGoemetry
+    """
+    def __init__(self, width, height):
+        """
+        initialization function
+        """
+        self.integer_validator("Width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Area of the rectangle
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Str Return
+        """
+        ptr = "[" + str(self.__class__.__name__) + "] "
+        ptr += str(self.__width) + "/" + str(self.__height)
+        return ptr
