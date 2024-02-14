a165dc291e36f06beb2d9de84af7360583028ac7
jennycode7 <jennessecosmas7@gmail.com>
2024-02-14 13:27:16 -0800

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,13 +5,13 @@ if (process.argv[2]) {
   if (isNaN(num)) {
     console.log('Missing size');
   } else {
-      for (let i = 0; i < num; i++) {
-        let y = 'X';
-        for (let x = 0; x < num - 1; x++) {
-          y += 'X';
-	}
-        console.log(y);
+    for (let i = 0; i < num; i++) {
+      let y = 'X';
+      for (let x = 0; x < num - 1; x++) {
+        y += 'X';
       }
+      console.log(y);
+    }
   }
 } else {
   console.log('Missing size');
