015722a105f0284497bb00e6a76c12fcadde84e4
jennycode7 <jennessecosmas7@gmail.com>
2024-02-16 06:06:27 -0800

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-  let x = 1;
-  let y = 1;
-  const num = process.argv[2];
-  while (x <= num) {
-    y *= x;
-    x = x + 1;
+function factorial(n) {
+  if (n === 0) {
+    return 1;
   }
-  console.log(y);
+  else {
+    return n * factorial(n - 1);
+  }
+}
+
+
+if (process.argv[2]) {
+  console.log(factorial(process.argv[2]));
 } else {
   console.log('1');
 }
