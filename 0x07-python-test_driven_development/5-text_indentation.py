9a595d12d157302603fc9a315bc676c610fc68f6
jennycode7 <jennessecosmas7@gmail.com>
2024-03-02 20:06:31 -0800

    indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,7 +13,7 @@ def text_indentation(text):
         TypeError: If text is not a string.
     """
     if not isinstance(text, str):
-        raise TypeError("text must be a string") 
+        raise TypeError("text must be a string")
 
     c = 0
     while c < len(text) and text[c] == ' ':
