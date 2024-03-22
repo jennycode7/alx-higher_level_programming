f9e016bbbcc78911352a8fd894f696aa07fa0ede
jennycode7 <jennessecosmas7@gmail.com>
2024-03-22 14:07:25 -0700

    11-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A module
+"""
+
+
+class Student:
+    """
+    A class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        A initialializer
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        To json
+        """
+        return self.__dict__
+    def reload_from_json(self, json):
+        for attr in json:
+            setattr(self, attr, json[attr])
