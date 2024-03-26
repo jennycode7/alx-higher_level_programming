cc23aad1393530eb76fcb93eeeec5dadf65809ae
jennycode7 <jennessecosmas7@gmail.com>
2024-03-26 15:10:56 -0700

    update args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,3 +127,20 @@ class Rectangle(Base):
         ptr += '/' + str(self.y) + ' - '
         ptr += str(self.width) + '/' + str(self.height)
         return ptr
+
+    def update(self, *args):
+    """
+    Update the rectangle
+    """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+        if len(args) >= 6:
+            raise TypeError('Too many arguments')
