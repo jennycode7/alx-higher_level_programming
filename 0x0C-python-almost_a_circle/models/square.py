b5dd8489249be487bb2367fbdb1b8527b57cc2dc
jennycode7 <jennessecosmas7@gmail.com>
2024-03-26 17:58:40 -0700

    addition

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A module
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class Square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize self
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Return a string representation of the square
+        """
+        ptr = '[Square]' + ' (' + str(self.id) + ')' + ' ' + str(self.x) + '/'
+        ptr += str(self.y) + ' - ' + str(self.width)
+        return ptr
