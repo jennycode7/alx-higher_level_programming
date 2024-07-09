f020cedee558a89d604e8719f9fe56ed3c170702
jennycode7 <jennessecosmas7@gmail.com>
2024-07-09 10:17:49 -0700

    semistandard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
 
-
 const FindSecondLargest = (...args) => {
-	const integers = args.map(Number).filter(num => Number.isInteger(num));
+  const integers = args.map(Number).filter(num => Number.isInteger(num));
 
-	if (integers.length === 0 || integers.length === 0)
-	{
-		console.log(0);
-		return;
-	}
+  if (integers.length === 0 || integers.length === 0) {
+    console.log(0);
+    return;
+  }
+  const sortedIntegers = integers.sort((a, b) => b - a);
 
-	const sortedIntegers = integers.sort((a, b) => b - a);
-
-	console.log(sortedIntegers[1]);
+  console.log(sortedIntegers[1]);
 };
 
 const args = process.argv.slice(2);
