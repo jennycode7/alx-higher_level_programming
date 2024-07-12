8501fbee955e81eecb5324631ebf881a0a0fb603
jennycode7 <jennessecosmas7@gmail.com>
2024-07-12 15:38:11 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,8 @@ def states_with_N(username, password, name):
                          passwd=password, db=name)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                   ORDER BY id ASC")
+    cursor.execute('''SELECT * FROM states'
+                   WHERE name LIKE BINARY 'N%' ORDER BY id ASC''')
     rows = cursor.fetchall()
 
     for row in rows:
