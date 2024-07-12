8afff1d8953a17100f9fb646bf047c7a7080e036
jennycode7 <jennessecosmas7@gmail.com>
2024-07-12 03:22:28 -0700

    sqlalchemy

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,8 @@
--- block
--- Convert the database to UTF8 (utf8mb4)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- Convert the table to UTF8 (utf8mb4)
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the column 'name' in the table 'first_table' to UTF8 (utf8mb4)
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- 0x0D. SQL - Introduction, task 17. Go to UTF8
+-- Converts DB `hbtn_0c_0`, table `first_table`, and field `name` to UTF-8.
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+/* ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256)
+   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; */
