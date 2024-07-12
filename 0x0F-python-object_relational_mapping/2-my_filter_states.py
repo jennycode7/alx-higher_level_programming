b13c08570d33be6990e81349a37e217a9e630b3a
jennycode7 <jennessecosmas7@gmail.com>
2024-07-12 16:13:46 -0700

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+A module
+'''
+import sys
+import MySQLdb
+
+
+def list_states(username, password, name, state_name):
+    '''
+    an function that prints a db
+    '''
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=name)
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4]
+
+    list_states(username, password, db, state)
